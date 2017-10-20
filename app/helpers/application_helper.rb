8591705e9ac5d18e0cafa55bf5a0af76db1bbd7a
vusiro <tieudaotac1996@gmail.com>
2017-10-20 07:49:25 +0700

    Make a basic User model

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,5 @@
 module ApplicationHelper
-  def full_title page_title = ''
+  def full_title page_title = ""
     base_title = t "ror_tut"
     page_title.empty? ? base_title: page_title + " | " + base_title
   end
