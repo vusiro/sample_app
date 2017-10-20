d8c8768a057677d596975ad700b03fb24e584f06
vusiro <tieudaotac1996@gmail.com>
2017-10-20 08:59:58 +0700

    Imlement basic login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,6 +8,7 @@ class UsersController < ApplicationController
    def create
    @user = User.new(user_params)
     if @user.save
+       log_in @user
        flash[:success] = "Welcome to the Sample App!"
        redirect_to @user
 
@@ -19,6 +20,6 @@ class UsersController < ApplicationController
 
    def user_params
       params.require(:user).permit(:name, :email, :password,
-                                   :password_confirmation)
+      :password_confirmation)
    end
 end
